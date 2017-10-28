d5cd08d5a94b7fb94c0f0af84f5446c542a42f09
Kaili Nishihira <Guest@epicodus-8.local>
2017-10-27 17:42:22 -0700

    add 3 most recent products

--- MariosSpecialtyProducts/Controllers/HomeController.cs
+++ MariosSpecialtyProducts/Controllers/HomeController.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MariosSpecialtyProducts.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MariosSpecialtyProducts.Controllers
 {
     public class HomeController : Controller
     {
+		private readonly MariosSpecialtyProductsContext _db;
+		public HomeController(MariosSpecialtyProductsContext db)
+		{
+			_db = db;
+		}
+
+
         public IActionResult Index()
         {
-            return View();
+            return View(_db.Products.OrderByDescending(x => x.ProductId).Take(3).ToList());
         }
 
         public IActionResult About()
